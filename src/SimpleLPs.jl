b7bb5d06d13de0388ce728312e55eb382db2d64a
Ed Scheinerman <ers@jhu.edu>
2025-08-29 15:58:26 -0400

    basics

--- src/SimpleLPs.jl
+++ src/SimpleLPs.jl
@@ -1,5 +1,63 @@
 module SimpleLPs
 
-greet() = print("Hello World!")
+using ChooseOptimizer
+using JuMP
+
+export LP, solve
+
+"""
+ LP(A, b, c; objective=:min, relation=:geq, nonneg=true, verbose=false)
+
+ Create a linear program.
+ """
+struct LP
+    A::AbstractMatrix
+    b::AbstractVector
+    c::AbstractVector
+
+    objective::Symbol     # one of :max or :min
+    relation::Symbol      # one of :leq :eq or :geq
+    nonneg::Bool          # if true, variables are nonnegative
+    verbose::Bool         # pass on to solver
+
+    function LP(A, b, c; objective=:min, relation=:geq, nonneg=true, verbose=false)
+        if !_size_check(A, b, c)
+            error("Size mismatch")
+        end
+
+        if !_objective_check(objective)
+            error("Unknown objective $objective. Use :max or :min")
+        end
+
+        if !_relation_check(relation)
+            error("Unknown relation $relation. Use :leq, :eq, or :geq")
+        end
+
+        new(A, b, c, objective, relation, nonneg, verbose)
+    end
+end
+
+function nvars(P::LP)
+    return length(P.c)
+end
+
+function ncons(P::LP)
+    length(P.b)
+end
+
+function _size_check(A, b, c)::Bool
+    m, n = size(A)
+    return length(b) == m && length(c) == n
+end
+
+function _objective_check(o::Symbol)::Bool
+    return o==:min || o==:max
+end
+
+function _relation_check(r::Symbol)::Bool
+    return r==:leq || r==:eq || r==:geq
+end
+
+include("solver.jl")
 
 end # module SimpleLPs
---
+++ src/solver.jl
@@ -0,0 +1,64 @@
+"""
+    solve(P::LP)
+
+Solve a linear program `P` returning the pair `(z, x)`
+where `z` is the optimum value and `x` is the optimal vector.
+
+If `P` is either unbounded or infeasible, that will be reported as an 
+information message and `nothing` will be returned. 
+"""
+function solve(P::LP)
+    MOD = Model(get_solver())
+
+    nv = nvars(P)
+    nc = ncons(P)
+
+    if P.nonneg
+        @variable(MOD, x[1:nv] >= 0)
+    else
+        @variable(MOD, x[1:nv])   # unconstrained
+    end
+
+    if P.objective == :min
+        @objective(MOD, Min, sum(P.c[i]*x[i] for i in 1:nv))
+    else
+        @objective(MOD, Max, sum(P.c[i]*x[i] for i in 1:nv))
+    end
+
+    rel = P.relation
+
+    for i in 1:nc
+        if rel == :leq
+            @constraint(MOD, sum(P.A[i, j] * x[j] for j in 1:(nv)) ≤ P.b[i])
+        elseif rel == :geq
+            @constraint(MOD, sum(P.A[i, j] * x[j] for j in 1:(nv)) ≥ P.b[i])
+        else
+            @constraint(MOD, sum(P.A[i, j] * x[j] for j in 1:(nv)) == P.b[i])
+        end
+    end
+
+    set_solver_verbose(P.verbose)
+
+    optimize!(MOD)
+    status = Int(termination_status(MOD))
+
+    if status == 1
+        obj_value = objective_value(MOD)
+        xx = JuMP.value.(x)
+        return obj_value, xx
+    end
+
+    if status == 2
+        @info "This linear program is infeasible."
+        return nothing
+    end
+
+    if status == 3
+        @info "This linear program is unbounded"
+        return nothing
+    end
+
+    @info "Unknown termination status"
+
+    return nothing
+end
